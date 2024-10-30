b408c48a70f2931cb8c4de6a98e43a09efc4c63c
Joel Alves Pereira <atejap05@yahoo.com.br>
2024-10-29 21:18:26 -0300

    Implement authentication handlers and update Navbar for server actions; enhance Tailwind CSS configuration with new themes and typography

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,30 +6,41 @@ import { auth, signIn, signOut } from "@/auth";
 const Navbar = async () => {
   const session = await auth();
 
+  // Server actions
+  const handleLogout = async () => {
+    "use server";
+    await signOut({ redirectTo: "/" });
+  };
+
+  // Server actions
+  const handleLogin = async () => {
+    "use server";
+    await signIn("github");
+  };
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src={Logo} alt="logo" width={144} height={30} priority />
         </Link>
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-5 text-black">
           {session && session.user ? (
             <>
               <Link href="/startup/create">
                 <span> Create</span>
               </Link>
-
-              <button onClick={signOut}>
-                <span>Logout</span>
-              </button>
+              <form action={handleLogout}>
+                <button type="submit">Logout</button>
+              </form>
               <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
           ) : (
-            <button onClick={signIn("github")}>
-              <span>Login</span>
-            </button>
+            <form action={handleLogin}>
+              <button type="submit">Login</button>
+            </form>
           )}
         </div>
       </nav>
