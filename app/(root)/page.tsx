7e1534c5a7f8cb6f96c347ffc6dbc4d569ce24de
Joel Alves Pereira <atejap05@yahoo.com.br>
2024-11-05 20:38:25 -0300

    add sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { type StartupCardType } from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +8,54 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: "2021-09-29T14:00:00Z",
+      views: 5,
+      author: { _id: "1", name: "John Doe Alves Perera" },
+      _id: "1",
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9ib3R8ZW58MHx8MHx8fDA%3D",
+      category: "Tech",
+      title: "Startup 1",
+    },
+    {
+      _createdAt: "2021-09-29T14:00:00Z",
+      views: 5,
+      author: { _id: "1", name: "John Doe Alves Perera" },
+      _id: "2",
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9ib3R8ZW58MHx8MHx8fDA%3D",
+      category: "Tech",
+      title: "Startup 1",
+    },
+    {
+      _createdAt: "2021-09-29T14:00:00Z",
+      views: 5,
+      author: { _id: "1", name: "John Doe Alves Perera" },
+      _id: "3",
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9ib3R8ZW58MHx8MHx8fDA%3D",
+      category: "Tech",
+      title: "Startup 1",
+    },
+    {
+      _createdAt: "2021-09-29T14:00:00Z",
+      views: 5,
+      author: { _id: "1", name: "John Doe Alves Perera" },
+      _id: "4",
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9ib3R8ZW58MHx8MHx8fDA%3D",
+      category: "Tech",
+      title: "Startup 1",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -16,6 +66,16 @@ export default async function Home({
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.map((post: StartupCardType) => (
+            <StartupCard key={post?._id} {...post} />
+          ))}
+        </ul>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+export type StartupCardType = {
+  _createdAt: string;
+  views: number;
+  author: { _id: string; name: string };
+  _id: string;
+  description: string;
+  image: string;
+  category: string;
+  title: string;
+};
+
+const StartupCard = ({
+  _createdAt,
+  views,
+  author,
+  _id,
+  description,
+  image,
+  category,
+  title,
+}: StartupCardType) => {
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1 5">
+          <EyeIcon className="size-6 text-primary"></EyeIcon>
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            src={"https://placehold.co/48x48"}
+            alt={"placeholder"}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        <img src={image} alt="image_post" className="startup-card_img" />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLocaleLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;
